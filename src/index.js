4f926c7ca8db3bd7ed8e0ba11efcffe172656fdd
unknown <hasib8nh@gmail.com>
2023-04-07 14:42:40 +0600

    add rating functionality

--- src/index.js
+++ src/index.js
@@ -8,21 +8,29 @@ const createArray = (length) => [
   ...Array(length)
 ];
 
-function Star({selected = false}){
-  return (
-  <FaStar color={selected ? "red": "gray"}/>
-  );
+function Star({selected = false, onSelect}){
+  return  <FaStar color={selected ? "red": "grey"}
+  onClick={onSelect}  
+  />
+
 }
 
-function StarRating ({totalStars=5}){
-  return createArray(totalStars).map((n, i) => (
-    <Star key={i} />
-  ));
+function StarRating({totalStars=5}) {
+  const [selectedStars, setSelectedStars] = useState(0);
+ return (
+ <>
+ {createArray(totalStars).map((n, i) => (
+  <Star key={i} selected= {selectedStars > i} onSelect={()=> setSelectedStars(i+1)}/>
+ ))}
+ <p>{selectedStars} of {totalStars}</p>
+ </>
+);
 }
+
 
 
 function App (){
-  return <StarRating totalStars = {10}/>
+  return <StarRating totalStars ={10}/>  
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
