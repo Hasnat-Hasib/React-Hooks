e9fd2fd8ce3571d7532287317679b22c7d46558b
unknown <hasib8nh@gmail.com>
2023-04-07 09:50:44 +0600

    useState()

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function App (){
+  const [status, setStatus] = useState("Not delivered");
+
+  return (
+    <div>
+      <h1>The package is: {status}.</h1>
+      <button onClick={ () => setStatus("Delivered")} >Deliver</button>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App  />
   </React.StrictMode>
 );
 
