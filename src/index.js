005a43aaf3c26ef97f7f36fd28cb7d67d7e5b635
unknown <hasib8nh@gmail.com>
2023-04-08 12:16:56 +0600

    Building fetch hooks

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-import React, {createContext, useContext} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {useFetch} from "./useFetch";
 
-const TreesContext = createContext();
-export const useTrees = () =>
-  useContext(TreesContext);
-
-const trees = [
-  {id: "1", type: "Maple"},
-  {id: "2", type: "Dak"},
-  {id: "3", type: "Family"},
-  {id: "4", type: "Component"},
-]
-
+function App({login}) {
+  const {loading, data, error} = useFetch(`https://api.github.com/users/${login}`);
+  
+  if(loading) return <h1>loading</h1>;
+  if(error) 
+  return (
+    <pre>{JSON.stringify(error, null, 2)}</pre>
+  );
+  return (
+    <div>
+      <img src="data.avatar_url" alt="data.login" />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TreesContext.Provider value={{trees}}>
-    <App  />
-  </TreesContext.Provider>
+ 
+    <App login="eveporcello"/>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
