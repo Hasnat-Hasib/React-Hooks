1c5184b1683c26a0102e6ef57fbcf091e6e98242
unknown <hasib8nh@gmail.com>
2023-04-08 08:54:52 +0600

    checkbox using useReducer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-
 
 function App (){
-  const [number, setNumber] = useReducer((number, newNumber) => number + newNumber, 0);
- 
-  return (
-    
-    <h1 onClick={() => setNumber(1)}>{number}</h1>
-    
-  );
+  const [checked, toggle] = useReducer((checked) => !checked, false);
 
-  console.log(number);
+  return (
+    <div>
+      
+      <input type="checkbox"
+      value={checked}
+      onChange={toggle} />
+      <p>{checked ? "checked": "not checked"}</p>
+    </div>
+  )
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -26,4 +27,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
